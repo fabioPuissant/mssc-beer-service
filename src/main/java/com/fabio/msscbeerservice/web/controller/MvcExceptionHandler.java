bcc438fbb8b0c0d39fa399e7fc2413af63c635a2
FabioP <fabio.puissant@hotmail.com>
2021-12-13 23:44:31 +0100

    Added validation to BeerDto.java

---
+++ src/main/java/com/fabio/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.fabio.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex){
+        List<String> errorList = new ArrayList<>(ex.getConstraintViolations().size());
+        ex.getConstraintViolations().forEach(error -> errorList.add(error.toString()));
+
+        return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
+    }
+}
